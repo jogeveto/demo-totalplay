e6bb6b27130b0b57533d12cff9a78ccbeb79c54e
Javier Castañeda <javier.c@montechelo.com.co>
2022-06-09 13:18:10 -0500

    Formulario Total play

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import Swal from 'sweetalert2';
+import { TotalService } from './services/total.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'demo-totalplay';
+export class AppComponent implements OnInit {
+  totalForm!: FormGroup;
+
+  constructor(
+    private service: TotalService,
+    private formBuilder: FormBuilder
+  ) { }
+
+  ngOnInit() {
+    this.totalForm = this.formBuilder.group({
+      nombre: new FormControl('', [Validators.required]),
+      telefono: new FormControl('', [Validators.required]),
+      direccion: new FormControl('', [Validators.required]),
+      tiempo: new FormControl('', [Validators.required, Validators.pattern('^[0-9]*$')]),
+      barrio: new FormControl('', [Validators.required]),
+      proveedor: new FormControl('', [Validators.required]),
+      codigo: new FormControl('', [Validators.required, Validators.pattern('^[0-9]*$')])
+    });
+  }
+
+  get f(): { [key: string]: AbstractControl } {
+    return this.totalForm.controls;
+  }
+
+  onSubmit(form: FormGroup) {
+    console.log('entro');
+    
+    const body = {
+      "producto": 'demo-total-play',
+      "token": "6cf2a746-af17-40c1-91d2-68d601a76f57",
+      "telefono": form.value.telefono,
+      "nombre-completo": form.value.nombre,
+      "direccion": form.value.direccion,
+      "barrio": form.value.barrio,
+      "proveedor-internet": form.value.proveedor,
+      "tiempo-proveedor": form.value.tiempo,
+      "codigo-promotor": form.value.codigo
+    };
+    this.service.SendForm(body).subscribe(resp => {
+      Swal.fire(
+        'Excelente!',
+        'Gracias por tomarse el tiempo para diligenciar el formulario.',
+        'success'
+      )
+    });
+  }
+
 }
---
+++ src/app/services/total.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TotalService {
+
+constructor(private http: HttpClient) { }
+
+SendForm(data: any){
+/*   const httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': 'true',
+      'Access-Control-Allow-Methods' : 'GET, POST, OPTIONS',
+      'Access-Control-Allow-Headers' : 'Origin, Content-Type, Accept, X-Custom-Header, Upgrade-Insecure-Requests',
+    })
+  }; */
+  return this.http.post(`https://app.outsourcingcos.com/webservice-dinamico/cos/services`, data);
+}
+
+}
